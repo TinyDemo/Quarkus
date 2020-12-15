a9e5100d27f4a33711f5d4108696c376adfd104a
where.liu <where.liu@huolala.cn>
2020-12-15 10:15:05 +0800

    feat: fist

--- src/main/java/cn/tinydemo/controller/IndexController.java
+++ src/main/java/cn/tinydemo/controller/IndexController.java
@@ -1,5 +1,12 @@
 package cn.tinydemo.controller;
 
+import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.info.Info;
+import org.eclipse.microprofile.openapi.annotations.links.Link;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -11,6 +18,13 @@ import javax.ws.rs.core.MediaType;
 @Path("/hello")
 public class IndexController {
 
+    /**
+     * 响应测试
+     *
+     * @return 测试字符串
+     */
+    @Operation(summary = "响应测试")
+    @Tag(name = "Index")
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
